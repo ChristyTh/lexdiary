23b65e882c3eb33e61a7c3e9eac3282998e0f145
akshay.sivam <akshaysivamofficial@gmail.com>
2025-12-18 21:10:42 +0530

    added .env file

--- lexdiary/settings.py
+++ lexdiary/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-insecure-key")
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG','False')=='True'
 
 ALLOWED_HOSTS = ["*"]
 
