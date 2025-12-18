4df0c218b4368a87ca2d0cce0447429ecfc6a8a0
akshay.sivam <akshaysivamofficial@gmail.com>
2025-12-18 22:07:41 +0530

    debugged code ready

--- lexdiary/settings.py
+++ lexdiary/settings.py
@@ -145,7 +145,9 @@ LOGIN_REDIRECT_URL = "/"
 # -------------------------------------------------
 STATIC_URL = "static/"
 
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT=BASE_DIR/'staticfiles'
 
 
 
